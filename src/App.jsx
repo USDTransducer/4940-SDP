db80042c0acf20ee4dc4b05db078c752e290ef54
USDTransducer <usdtransducerproject@gmail.com>
2023-02-23 13:47:29 -0500

    added Systemlog to the web page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Chart from 'react-google-charts'
 import Navbar from './components/Navbar';
 import ChartDisplay from './components/ChartDisplay';
 import Controls from './components/Controls';
-
+import Sidebar from './components/Sidebar';
+import Systemlog from './components/Systemlog';
 function App() {
 
   return (
     <div className="website">
       <Navbar />
-      <div className="flex flex-row mx-auto px-10 bg-green-300">
-        <div className='basis-1/4 md:basis-1/4 px-4'> <Controls /> </div>
+      <div className="flex flex-row mx-auto px-10 bg-gray-300">
+        <div className='basis-1/4 md:basis-1/4 px-4'> <Sidebar /> </div>
         <div className='basis-1/2 md:basis-1/2 px-4'> <ChartDisplay /> </div>
-        <div className='basis-1/4 md:basis-1/4 px-4'> System Log </div>
+        <div className='basis-1/4 md:basis-1/4 px-4'> <Systemlog /> </div>
       </div>
     </div>
   )
