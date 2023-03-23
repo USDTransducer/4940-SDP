7c338e3c0aee03b5ef10264d3c1a81e417f2f3d3
USDTransducer <usdtransducerproject@gmail.com>
2023-03-23 13:04:27 -0400

    Graph is now connected to the google sheets api Todo: Figure out how to make the x axis use the timestamp - Might need to parse the json timestamp somehow Todo: Figure out spacing between components and graph scale

--- src/components/ChartDisplay.jsx
+++ src/components/ChartDisplay.jsx
@@ -1,64 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 
-
-
-export const data = [
-  [
-    { type: "number", label: "x" },
-    { type: "number", label: "values" },
-    { id: "i0", type: "number", role: "interval" },
-    { id: "i1", type: "number", role: "interval" },
-    { id: "i2", type: "number", role: "interval" },
-    { id: "i2", type: "number", role: "interval" },
-    { id: "i2", type: "number", role: "interval" },
-    { id: "i2", type: "number", role: "interval" },
-  ],
-  [1, 100, 90, 110, 85, 96, 104, 120],
-  [2, 120, 95, 130, 90, 113, 124, 140],
-  [3, 130, 105, 140, 100, 117, 133, 139],
-  [4, 90, 85, 95, 85, 88, 92, 95],
-  [5, 70, 74, 63, 67, 69, 70, 72],
-  [6, 30, 39, 22, 21, 28, 34, 40],
-  [7, 80, 77, 83, 70, 77, 85, 90],
-  [8, 100, 90, 110, 85, 95, 102, 110],
-];
-
-export const options = {
-  title: "Current over time",
-  curveType: "function",
-  series: [{ color: "#E7711B" }],
-  intervals: { style: "area" },
-  legend: "none",
-};
-
 function ChartDisplay() {
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://sheet.best/api/sheets/794f1ec2-1029-4bb3-94cb-34e96fe5a0cd");
+        const data = await response.json();
+        const chartData = [["Id", "Current"]];
+        data.forEach((item) => {
+          chartData.push([parseInt(item.Id), parseInt(item.Current)]);
+        });
+        console.log(chartData)
+        setChartData(chartData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <Chart
-        chartType="LineChart"
-        width="100%"
-        height="400px"
-        data={data}
-        options={options}
-      />
-
-      <button type="button" 
-      class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      style = {{width: "50%"}}> Last 5 Days </button>
-
-      <button type="button" 
-      class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      style = {{width: "50%"}}> Hourly </button>
-
-      <button type="button" 
-      class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      style = {{width: "50%"}}>Apply</button>
-
-
+    <div>
+      <h3>Chart:</h3>
+      {chartData && (
+        <Chart
+          width={"500px"}
+          height={"500px"}
+          chartType="LineChart"
+          data={chartData}
+          options={{
+            title: "Comments",
+            chartArea: { width: "50%" },
+            hAxis: { title: "Id", minValue: 0 },
+            vAxis: { title: "Current" },
+          }}
+        />
+      )}
     </div>
   );
 }
-
 export default ChartDisplay;
-
