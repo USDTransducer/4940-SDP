5e2a5c834f8590abe7077521ae5116df549e1f86
USDTransducer <usdtransducerproject@gmail.com>
2023-04-06 07:24:17 -0400

    Made the chart functional and makes sense to look at To do for chart:
    Add controls,
    Adjust visuals,
    hours, seconds, minutes, for intervals after the time like 2hrs, etc
    
    Next steps:
    - Arduino needs to format the data to the spreadsheet properly
    - website needs to send which user requested data and post that to the
    spreadsheet script as well as having 'system' as the default user as to
    not leave that row blank for the user column
    
    Log in system
    User-auth
    
    System Log
    
    Arduino controls

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import ChartDisplay from './components/ChartDisplay';
 import Controls from './components/Controls';
 import Sidebar from './components/Sidebar';
 import Systemlog from './components/Systemlog';
+
 function App() {
 
   return (
     <div className="website">
       <Navbar />
-      <div className="flex flex-row mx-auto px-10 bg-gray-300">
-        <div className='basis-1/4 md:basis-1/4 px-4'> <Sidebar /> </div>
-        <div className='basis-1/2 md:basis-1/2 px-4'> <ChartDisplay /> </div>
-        <div className='basis-1/4 md:basis-1/4 px-4'> <Systemlog /> </div>
+      <div className="flex flex-col md:flex-row mx-auto px-10 bg-gray-300">
+        <div className='md:basis-1/4 px-4'> <Sidebar /> </div>
+        <div className='md:basis-1/2 px-4 mr-4 md:mr-0' style={{ maxWidth: '100%', margin: 'auto' }}> <ChartDisplay /> </div>
+        <div className='md:basis-1/4 px-4'> <Systemlog /> </div>
       </div>
     </div>
   )
--- src/components/ChartDisplay.jsx
+++ src/components/ChartDisplay.jsx
@@ -1,19 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
+import MyChart from "./MyChart";
 
 function ChartDisplay() {
   const [chartData, setChartData] = useState(null);
+  const [dates, setDates] = useState([]);
+  const [times, setTimes] = useState([]);
+  const [currents, setCurrents] = useState([]);
+  const [types, setTypes] = useState([]);
+  const [intervals, setIntervals] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
+    console.log("dates", dates);
     async function fetchData() {
       try {
         const response = await fetch("https://sheet.best/api/sheets/794f1ec2-1029-4bb3-94cb-34e96fe5a0cd");
         const data = await response.json();
-        const chartData = [["Id", "Current"]];
+        console.log(response)
+        const newDates = [];
+        const newTimes = [];
+        const newCurrents = [];
+        const newTypes = [];
+        const newIntervals = [];
+        const newUsers = [];
+
         data.forEach((item) => {
-          chartData.push([parseInt(item.Id), parseInt(item.Current)]);
+          newDates.push(item.Date);
+          newTimes.push(item.Time);
+          newCurrents.push(parseInt(item.Current));
+          newTypes.push(parseInt(item.Type));
+          newIntervals.push(parseInt(item.Interval));
+          newUsers.push(item.User);
         });
-        console.log(chartData)
+
+        setDates(newDates);
+        setTimes(newTimes);
+        setCurrents(newCurrents);
+        setTypes(newTypes);
+        setIntervals(newIntervals);
+        setUsers(newUsers);
+        console.log("Diagnostic");
+        const chartData = [["Date","Time", "Current","Type","Interval","User"]];
+        console.log(chartData);
+        data.forEach((item) => {
+          chartData.push([item.Date, item.Time, parseInt(item.Current), parseInt(item.Type), parseInt(item.Interval)], item.User);
+        });
+        console.log(chartData);
+        console.log("dates");
+        console.log(dates);
         setChartData(chartData);
       } catch (error) {
         console.log(error);
@@ -24,21 +59,8 @@ function ChartDisplay() {
 
   return (
     <div>
-      <h3>Chart:</h3>
-      {chartData && (
-        <Chart
-          width={"500px"}
-          height={"500px"}
-          chartType="LineChart"
-          data={chartData}
-          options={{
-            title: "Comments",
-            chartArea: { width: "50%" },
-            hAxis: { title: "Id", minValue: 0 },
-            vAxis: { title: "Current" },
-          }}
-        />
-      )}
+      <MyChart dates={dates} times={times} currents={currents} types={types} intervals={intervals} users={users}/>
+
     </div>
   );
 }
