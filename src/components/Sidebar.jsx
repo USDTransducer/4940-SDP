712498055a3973a13227590e821bc662c77cd4b8
USDTransducer <usdtransducerproject@gmail.com>
2023-04-17 19:00:13 -0400

    Pop up modal for Change Interval button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,29 +2,6 @@ import React, { useState } from 'react';
 import { Modal } from 'flowbite';
 import axios from 'axios'; // import axios to make HTTP request
 
-function ChangeIntervalButton() {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  function handleClick() {
-    setModalVisible(true);
-  }
-
-  return (
-    <>
-      <button type="button" onClick={handleClick}>Change Interval</button>
-      {modalVisible &&
-        <div className="modal">
-          <form>
-            <label htmlFor="intervalInput">Enter a new interval:</label>
-            <input type="text" id="intervalInput" />
-            <button type="submit">Submit</button>
-            <button type="cancel">Cancel</button>
-          </form>
-        </div>
-      }
-    </>
-  );
-}
 //staging changes
 const Sidebar = () => {
 
@@ -59,15 +36,49 @@ const Sidebar = () => {
         <p class="text-sm font-normal text-white dark:text-gray-400">Uptime: 15 days 10 hours</p>
         <ul class="my-4 space-y-3">
             <li>
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">                  
-            Restart Device
-                </button>
+            
             </li>
             <p class="text-sm font-normal text-white dark:text-gray-400">Next Data Reading: 10 minutes</p>
 
             <li>
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              <ChangeIntervalButton> Change interval </ChangeIntervalButton></button>
+            <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+              Change Interval
+            </button>
+            <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 flex justify-center items-center h-screen hidden">
+                <div class="relative w-full max-w-md max-h-full">
+
+                  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="px-6 py-6 lg:px-8">
+                            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Change Interval</h3>
+                            <form class="space-y-6" action="#">
+                                <div>
+                                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required></input>
+                                </div>
+                                <div class="flex justify-between"> <div class="flex items-start"> </div> </div>
+                                  <div class="text-center">
+                                      <div class="inline-flex rounded-md shadow-sm" role="group">
+                                        <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-l-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                          Minutes
+                                        </button>
+                                        <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                          Hours
+                                        </button>
+                                      </div>
+                                    </div>
+
+
+
+                                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Set Interval</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+  
             </li>
 
             <p class="text-sm font-normal text-white dark:text-gray-400">Last Reading: 60 seconds</p>
