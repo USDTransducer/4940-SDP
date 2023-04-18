78c5f410737c39adc2bfb29ba8c681da3ad28e3e
USDTransducer <usdtransducerproject@gmail.com>
2023-04-17 21:06:08 -0400

    authorization 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,22 @@ import Controls from './components/Controls';
 import Sidebar from './components/Sidebar';
 import Systemlog from './components/Systemlog';
 import Login from './components/Login';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 
 // client_id: "1061531809258-v4845u688g64cv91f56jcm2m9et9k8p7.apps.googleusercontent.com"
 function App() {
+  const [user, setUser] = useState({});
+
   function hcbResponse(response){
-    console.log("JTW",response.credential)
+    console.log("JTW",response.credential);
+    var uObject = jwt_decode(response.credential);
+    setUser(uObject);
+    document.getElementById("signDiv").hidden = true;
+  }
+  function signOut(event){
+    setUser({});
+    document.getElementById("signDiv").hidden = false;
   }
   useEffect(() => {
     /* global google */
@@ -30,7 +40,7 @@ function App() {
     <div className="website" style={{ backgroundColor: "#111827" }}>
       <Navbar />
       <div className="flex flex-col md:flex-row px-10" style={{ height: "100vh" , backgroundColor: "#111827"}}>
-        <div className='md:basis-1/4 px-4' style={{maxWidth: '50%', margin: 50, height: 50}}> <Sidebar /> </div>
+        <div className='md:basis-1/4 px-4' style={{maxWidth: '50%', margin: 50, height: 50}}> <Sidebar user={user} signOut={signOut}/> </div>
         <div className='md:basis-1/2 px-4 mr-4 md:mr-0' style={{ maxWidth: '100%', margin: 'auto' }}> <ChartDisplay /> </div>
         <div className='md:basis-1/4 px-4'style={{maxWidth: '50%', margin: 50, height: 50}}> <Systemlog /> </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-function Navbar()
+
+function Navbar({user,signOut})
 {
     return(
         <nav className="px-2 py-1 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700" style={{backgroundColor: "#1F2937"}}> 
@@ -9,6 +10,9 @@ function Navbar()
                 <span class="self-center text-xl whitespace-nowrap dark:text-white text-white">IoT Wireless Transducer</span>
             </a>
                 <div id="signDiv"></div>
+                {user != null &&
+                    <button onClick = { (e) => signOut(e)}> Sign Out</button>
+                }
                 <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
