a461a96dff760fe885cfd698b0dcdf8971676dda
USDTransducer <usdtransducerproject@gmail.com>
2023-04-19 16:08:58 -0400

    Auth button

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,57 @@ import Sidebar from './components/Sidebar';
 import Systemlog from './components/Systemlog';
 import { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
+import Cookies from 'js-cookie';
+import GoogleButton from 'react-google-button';
+
 
 function App() {
   const [user, setUser] = useState({});
 
-  function hcbResponse(response){
+  function hcbResponse(response) {
     var uObject = jwt_decode(response.credential);
     setUser(uObject);
+    Cookies.set('jwt_token', response.credential, { expires: 7 });
     console.log("Logging in User: ", uObject);
     document.getElementById("signInButton").hidden = true;
   }
-  
-  function signOut(event){
+
+  function signOut(event) {
     console.log("Logging out User: ", user);
     setUser({});
+    Cookies.remove('jwt_token');
     document.getElementById("signInButton").hidden = false;
   }
-  
+
+  useEffect(() => {
+    const jwtToken = Cookies.get('jwt_token');
+    if (jwtToken) {
+      const uObject = jwt_decode(jwtToken);
+      setUser(uObject);
+      console.log("User is already signed in: ", uObject);
+      document.getElementById("signInButton").hidden = true;
+    }
+  }, []);
+
   useEffect(() => {
     google.accounts.id.initialize({
       client_id: "1061531809258-v4845u688g64cv91f56jcm2m9et9k8p7.apps.googleusercontent.com",
       callback: hcbResponse
     })
-  
+
     google.accounts.id.renderButton(
       document.getElementById("signInButton"),
-      {theme:"outline",size:"medium"}
+      { theme: "outline", size: "medium" }
     );
   }, []);
 
   useEffect(() => {
     console.log("User update:", user);
   }, [user]);
-  
+
   return (
     <div className="website" style={{ backgroundColor: "#111827" }}>
-      <Navbar user={user} signOut={signOut} />
+      <Navbar user ={user} signOut={signOut}/>
       <div className="flex flex-col md:flex-row px-10" style={{ height: "100vh" , backgroundColor: "#111827"}}>
         <div className='md:basis-1/4 px-4' style={{maxWidth: '50%', margin: 50, height: 50}}> <Sidebar user={user} signOut={signOut}/> </div>
         <div className='md:basis-1/2 px-4 mr-4 md:mr-0' style={{ maxWidth: '100%', margin: 'auto' }}> <ChartDisplay /> </div>
@@ -50,4 +65,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
-import { render } from 'react-dom';
 
-function Navbar(props) {
-    const { user, signOut } = props;
+function Navbar({ user, signOut }) {
 
-    function renderSignIn(){
-        if (!user.email){
-            document.getElementById("signInButton").hidden = true;
-        }
-        else{
-            document.getElementById("signInButton").hidden = false;
-        }
-    }
+  function GoogleButton({ user, signOut }) {
+    return user.email ? (
+      <>
+        <span className="text-white mr-4">{user.email}</span>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={signOut}
+        >
+          Sign Out
+        </button>
+      </>
+    ) : (
+      <div id="signInButton"></div>
+    );
+  }
 
-    return (
-        <nav className="px-2 py-1 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700" style={{ backgroundColor: "#1F2937" }}>
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
-          <a href="#" className="flex items-center">
+  return (
+    <nav className="px-2 py-1 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700" style={{ backgroundColor: "#1F2937" }}>
+      <div className="container flex items-center justify-between mx-auto">
+        <div className="flex items-center">
+          <a href="/" className="flex items-center">
             <img src="https://www.engr.uconn.edu/wp-content/uploads/2019/01/uconn-soe1-300x108.png" className="h-6 mr-3 sm:h-10" alt="Logo" />
             <span className="self-center text-xl whitespace-nowrap dark:text-white text-white">IoT Wireless Transducer</span>
           </a>
-          {user.email ? (
-            <>
-              <span className="text-white mr-4">{user.email}</span>
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                onClick={signOut}
-              >
-                Sign Out
-              </button>
-            </>
-          ) : (
-            <div id="signInButton"></div>
-          )}
         </div>
-      </nav>
-    );
-  }
+        <GoogleButton user={user} signOut={signOut} />
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
